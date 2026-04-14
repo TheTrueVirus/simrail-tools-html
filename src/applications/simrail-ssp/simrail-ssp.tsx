f69c671ecc4f5dbfed6e3db06888d11dc4d0eb75
TheTrueVirus <thetruevirusyt@gmail.com>
2026-04-14 19:38:37 +0200

    # Several changes and additions - changed data type of devTrain to FilteredTrainList / changed values in const - change areaDisplayTitle to visible tracks (Lazy LA) - changed names of state getter and setter and updated them in functions - added function to create own trainList (filtered only necessary values) - reworked const 'sspOptions'    - renamed all values and updated state getter and setter to new names - updated module props of 'SimRailSSP_Header' and added sspOptions - removed module SSP_OptionsMenu'

--- src/applications/simrail-ssp/simrail-ssp.tsx
+++ src/applications/simrail-ssp/simrail-ssp.tsx
@@ -16,47 +16,38 @@ const DISCLAIMER_KEY = "ssp_disclaimer_alpha0.0.1"
 
 export default function SimRailStreckenspiegel() {
 
-    const developmentTrain: SimRailDataTypes.TrainData = {
+    const developmentTrain: SimRailDataTypes.FilteredTrainList = {
         TrainNoLocal: 'DEVDEV',
-        TrainName: 'DEV',
+        Type: 'DEV',
         StartStation: 'Katowice',
         EndStation: 'Warszawa',
-        Vehicles: [],
-        ServerCode: 'de1',
         TrainData: {
-            ControlledBySteamID: null,
-            ControlledByXboxID: null,
-            InBorderStationArea: true,
-            Latititute: 50.0,
-            Longitute: 30.0,
-            Velocity: 40,
             SignalInFront: 'KO_E8',
             DistanceToSignalInFront: 100,
             SignalInFrontSpeed: 0,
-            VDDelayedTimetableIndex: 0,
-            RequiredMapDLCs: null
         },
-        RunId: '',
-        id: '',
-        Type: 'user'
+        ControlledBy: 'user'
     }
 
     const areaList = [
         {
             areaID: 'sspArea1',
-            areaDisplayTitle: 'P1 | Katowice - [Lazy]'
+            areaDisplayTitle: 'P1 | Katowice - [Lazy LA]'
         },
     ]
 
-    const [GET_serverList, SET_serverList] = useState<SimRailDataTypes.ServerData[]>([]);
-    const [GET_stationList, SET_stationList] = useState<SimRailDataTypes.StationData[]>([]);
-    const [fetchedTrains, SET_fetchedTrains] = useState<SimRailDataTypes.TrainData[]>([]);
-    const [selectedServer, SET_selectedServer] = useState<string>('int1');
-    const [GET_areaList] = useState<AreaProps[]>(areaList);
-    const [selectedArea, SET_selectedArea] = useState<AreaProps>(areaList[0])
-    const [isShowLongStationNames, SET_showLongStationsNames] = useState<boolean>(true);
-    const [isShowTestTrains, setShowTestTrains] = useState<boolean>(false);
+    const [serverList, setServerList] = useState<SimRailDataTypes.ServerData[]>([]);
+    const [stationList, setStationList] = useState<SimRailDataTypes.StationData[]>([]);
+    const [trainList, setTrainList] = useState<SimRailDataTypes.FilteredTrainList[]>([]);
+    
     const [showDisclaimer, setShowDisclaimer] = useState<boolean>(false);
+    //!!! make an option object, save it to localStorage and read localStorage onLoad in useEffect
+    const [selectedServer, setServer] = useState<string>('int1');
+    const [areaListState] = useState<AreaProps[]>(areaList);
+    const [selectedArea, setArea] = useState<AreaProps>(areaList[0])
+    const [isShowLongStationNames, SET_showLongStationsNames] = useState<boolean>(false);
+    const [isShowTestTrains, setShowTestTrains] = useState<boolean>(false);
+    const [showHeader, setShowHeader] = useState<boolean>(true);
 
     useEffect(() => {
         const accepted = localStorage.getItem(DISCLAIMER_KEY) === "true";
@@ -73,7 +64,7 @@ export default function SimRailStreckenspiegel() {
 
                 if (!DATA) return;
 
-                SET_serverList(DATA);
+                setServerList(DATA);
             } catch (e) {
                 console.error(e)
             }
@@ -94,7 +85,24 @@ export default function SimRailStreckenspiegel() {
 
             if (!TRAINDATA) return;
 
-            SET_fetchedTrains(TRAINDATA)
+            const filteredTrainList = () => {
+                return TRAINDATA.map((train) => {
+                    return {
+                        TrainNoLocal: train.TrainNoLocal,
+                        Type: train.TrainName,
+                        StartStation: train.StartStation,
+                        EndStation: train.EndStation,
+                        TrainData: {
+                            SignalInFront: train.TrainData.SignalInFront,
+                            SignalInFrontSpeed: train.TrainData.SignalInFrontSpeed,
+                            DistanceToSignalInFront: train.TrainData.DistanceToSignalInFront
+                        },
+                        ControlledBy: train.Type
+                    }
+                })
+            }
+
+            setTrainList(filteredTrainList())
         }
         getSimRailTrainData();
 
@@ -111,39 +119,44 @@ export default function SimRailStreckenspiegel() {
 
             if (!STATIONDATA) return;
 
-            SET_stationList(STATIONDATA)
+            setStationList(STATIONDATA)
         }
 
         return () => clearInterval(intervalID)
     }, [selectedServer])
 
     const finalTrainList = process.env.NODE_ENV === 'development'
-        ? [...fetchedTrains, developmentTrain]
-        : fetchedTrains;
+        ? [...trainList, developmentTrain]
+        : trainList;
 
-    const SSP_OPTIONS = {
-        OPTIONS_SERVER: {
-            serverList: GET_serverList,
+    const sspOptions = {
+        serverOptions: {
+            serverList,
             getServer: selectedServer,
-            setServer: SET_selectedServer
+            setServer
         },
-        OPTIONS_AREA: {
-            areaList: GET_areaList,
+        areaOptions: {
+            areaList: areaListState,
             getArea: selectedArea,
-            setArea: SET_selectedArea
+            setArea
         },
-        OPTION_SHOWSTATIONNAMES: {
+        stationNameOptions: {
             isShowLongStationNames,
             SET_showLongStationsNames,
         },
-        OPTION_SHOWTESTTRAINS: {
+        testTrainOptions: {
             isShowTestTrains,
             setShowTestTrains
+        },
+        showHeaderOptions: {
+            showHeader,
+            setShowHeader,
         }
     }
+
     const SSP_SVG_ITEMS = {
         trainList: finalTrainList,
-        stationList: GET_stationList,
+        stationList,
         selectedArea,
         isShowLongStationNames,
         isShowTestTrains,
@@ -154,11 +167,15 @@ export default function SimRailStreckenspiegel() {
         <>
             <div className='sspContainer'>
                 {showDisclaimer &&
-                    <SimRailSSP_Disclaimer DISCLAIMER_KEY={DISCLAIMER_KEY} showDisclaimer={showDisclaimer} setShowDisclaimer={setShowDisclaimer} />
+                    <SimRailSSP_Disclaimer
+                        DISCLAIMER_KEY={DISCLAIMER_KEY}
+                        showDisclaimer={showDisclaimer}
+                        setShowDisclaimer={setShowDisclaimer}
+                    />
                 }
-                <SimRailSSP_Header/>
+                <SimRailSSP_Header sspOptions={sspOptions} />
                 <SimRailSSP_SVG SSP_SVG_ITEMS={SSP_SVG_ITEMS} />
-                <SSP_OptionsMenu SSP_OPTIONS={SSP_OPTIONS} />
+                {/* <SSP_OptionsMenu SSP_OPTIONS={SSP_OPTIONS} /> */}
 
             </div>
         </>
