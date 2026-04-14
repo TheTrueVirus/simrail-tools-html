97cfaf9b1a2c77e0486c21f27c74fa0e78af483c
TheTrueVirus <thetruevirusyt@gmail.com>
2026-04-14 19:44:10 +0200

    - small refactor of some lines

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import Main from './main-page/main';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import CustomErrorHandler from './functions/customErrorHandler/errorHandler';
+import reportWebVitals from './reportWebVitals';
 import SimRailStreckenspiegel from './applications/simrail-ssp/simrail-ssp';
-import SideBar from './main-page/sidebar/sidebar';
+import './index.css';
+// import CustomErrorHandler from './functions/customErrorHandler/errorHandler';
+// import Main from './main-page/main';
+// import SideBar from './main-page/sidebar/sidebar';
+// import { AppProviders } from './functions/appcontext/appcontext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <div className='index'>
-      <div className='index_routes'>
-        <BrowserRouter basename='/simrail-tools-html'>
-          <div className='index_main'>
-            {/* <SideBar /> */}
-          </div>
-          <Routes>
-            <Route path='/' element={<SimRailStreckenspiegel />} />
-          </Routes>
-        </BrowserRouter>
+      <div className='index'>
+        <div className='index_routes'>
+          <BrowserRouter basename='/simrail-tools-html'>
+            <div className='index_main'>
+              {/* <SideBar /> */}
+            </div>
+            <Routes>
+              <Route path='/' element={<SimRailStreckenspiegel />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
       </div>
-    </div>
   </React.StrictMode>
 );
 reportWebVitals();
